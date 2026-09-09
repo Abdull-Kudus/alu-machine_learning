af35c749b387f93e3a55ed09a812e5e30e859aa4
Abdull-Kudus <a.zakariam@alustudent.com>
2026-09-09 03:30:04 +0200

    Fix linear_algebra scripts based on project requirements

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -16,7 +16,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
         _type_: _description_
     """
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
-        return mat1 + mat2
+        return [row[:] for row in mat1] + [row[:] for row in mat2]
     elif axis == 1 and len(mat1) == len(mat2):
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
     else:
